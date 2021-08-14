932c98c556a9fd3aa4c11a0a3a6fd0dfd9a9539e
Soham Samanta <sohamsamanta2@gmail.com>
2021-08-14 10:39:13 +0530

    1st update

---
+++ app/src/main/java/com/example/mangomusic/Splash.java
@@ -0,0 +1,25 @@
+package com.example.mangomusic;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 2000);
+    }
+}
